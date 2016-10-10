45a907ed205252cb4e5bea916f0a7931594bfaef
Bendik Solheim <bendik@solheim.im>
2016-10-10 23:01:12 +0200

    Improve Myo handling code
    - Add a threshold to the direction change
    - Draw magic number out to variables

--- myo.js
+++ myo.js
@@ -2,16 +2,23 @@ const myo = require('myo');
 const throttle = require('lodash/throttle');
 const math = require('./math');
 
-const changeFactor = 5;
+const change_factor = 5;
+const roll_threshold = 0.2;
+
+const speed_min = -1.0;
+const speed_max = -0.25;
 
 myo.on('orientation', (quaternion) => {
-    const rollValue = math.roll(quaternion) * -1;
+    var rollValue = math.roll(quaternion) * -1;
+    if (rollValue >= -roll_threshold && rollValue <= roll_threshold) {
+        rollValue = 0.0;
+    }
     const rollFactor = math.clamp(rollValue, -1, 1);
-    const rollDelta = Math.round(changeFactor * rollFactor);
+    const rollDelta = Math.round(change_factor * rollFactor);
     process.emit('myo:oriencationchange', rollDelta);
 
-    const speedValue = math.clamp(math.pitch(quaternion) * -1, -0.5, .5);
-    const speed = math.mapInterval(speedValue, -0.5, 0.5, 0, 150);
+    const speedValue = math.clamp(math.pitch(quaternion), speed_min, speed_max);
+    const speed = math.mapInterval(speedValue, speed_min, speed_max, 0, 150);
     process.emit('myo:speed', speed);
 });
 
